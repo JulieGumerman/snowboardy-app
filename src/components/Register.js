23233c021ba81ba56e8588456712b946ab1d0a3f
bodhibrdr <julie.gumerman@gmail.com>
2019-11-15 13:24:21 -0700

    register works

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+
+    const [newUser, setNewUser ] = useState({ username: "", password: ""});
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setNewUser({...newUser, [name]: value})
+        console.log(newUser);
+
+    }
+
+    const handleSubmit = (e, creds) => {
+        e.preventDefault();
+        console.log("HandleSubmitFunctionWHeeee", creds);
+        axios.post("https://snowboardy-life.herokuapp.com/api/register", creds)
+            .then(user => {
+                console.log("Hello from inside handleSubmit then statement")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     return (
         <div className="content-wrapper">
             Haii register screen!
-            <form className="log-reg-form">
+            <form className="log-reg-form" onSubmit={e => handleSubmit(e, newUser)}>
                 <div className="form-holder">
-                <input />
-                <input />
+                <input 
+                    placeholder="username"
+                    name="username"
+                    value={newUser.username}
+                    type="text"
+                    onChange={handleChange}
+                />
+                <input 
+                    placeholder="password"
+                    type="password"
+                    name="password"
+                    value={newUser.password}
+                    onChange={handleChange}
+                />
                 </div>
                 <button className="log-reg-button">Register</button>
             </form>    

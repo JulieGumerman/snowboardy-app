dd67e9cb62ded3b890d067ddea62507d7df9f4ba
bodhibrdr <julie.gumerman@gmail.com>
2019-11-15 13:07:32 -0700

    redux funk fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
-import { createStore, applyMiddleware } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 import { reducer } from "./reducers/index";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";

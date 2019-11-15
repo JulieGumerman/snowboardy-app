f396f127aab3c439db31820f82ca18efbf5488e9
bodhibrdr <julie.gumerman@gmail.com>
2019-11-15 13:00:10 -0700

    store set up in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+
+import { createStore, applyMiddleware } from "react-redux";
+import { reducer } from "./reducers/index";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
 import { BrowserRouter as Router} from "react-router-dom";
 
+const store = createStore(reducer, applyMiddleware(thunk));
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+
+ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
 

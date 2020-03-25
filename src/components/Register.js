48397fab954f54255d1d0a32476b08100b2017e7
bodhibrdr <julie.gumerman@gmail.com>
2020-03-25 17:50:45 -0600

    it's not pretty, but the validation works

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,32 +7,71 @@ const Register = (props) => {
 
     const [newUser, setNewUser ] = useState({ username: "", password: ""});
 
-    const handleChange = e => {
-        const {name, value} = e.target;
-        setNewUser({...newUser, [name]: value})
+    // const handleChange = e => {
+    //     const {name, value} = e.target;
+    //     setNewUser({...newUser, [name]: value})
 
-    }
+    // }
 
-    let registration_error = "NONE";
 
-    const handleSubmit = (e, creds) => {
-        e.preventDefault();
-        console.log("HandleSubmitFunctionWHeeee", creds);
-        axios.post("https://snowboardy-life.herokuapp.com/api/register", creds)
-            .then(user => {
-                props.history.push("/")
-            })
-            .catch(err => {
-                console.log(err)
-                registration_error = "YES"
-            })
-    }
+    // const handleSubmit = (e, creds) => {
+    //     e.preventDefault();
+    //     console.log("HandleSubmitFunctionWHeeee", creds);
+    //     axios.post("https://snowboardy-life.herokuapp.com/api/register", creds)
+    //         .then(user => {
+    //             props.history.push("/")
+    //             console.log(creds)
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //         })
+    // }
 
     return (
         <div className="content-wrapper">
             Register here.
-            <Formik>
-            <form className="log-reg-form" onSubmit={e => handleSubmit(e, newUser)}>
+            <Formik
+                initialValues={{username: '', password: ''}}
+                validate={values => {
+                    const errors = {};
+                    if (!values.username.length) {
+                        errors.username = "Required"
+                    }
+                    if (!values.password) {
+                        errors.password = "Required"
+                    }
+                    return errors;
+                }}
+                onSubmit={( values, {setSubmitting}) => {          
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                    }, 400)
+                }}
+            >
+                {({values, errors, touched, handleChange, handleSubmit, isSubmitting}) => (
+                    <form className="log-reg-form" onSubmit={e => handleSubmit(e, newUser)}>
+                        <div className="form-holder">
+                        <input 
+                            // placeholder="username"
+                            name="username"
+                            value={values.username}
+                            type="text"
+                            onChange={handleChange}
+                        />
+                        {errors.username && touched.username && errors.username}
+                        <input 
+                            // placeholder="password"
+                            type="password"
+                            name="password"
+                            value={values.password}
+                            onChange={handleChange}
+                        />
+                        {errors.username && touched.username && errors.username}
+                        </div>
+                        <button className="log-reg-button" type="submit" disabled={isSubmitting}>Register</button>
+                    </form>
+                )}
+            {/* <form className="log-reg-form" onSubmit={e => handleSubmit(e, newUser)}>
                 <div className="form-holder">
                 <input 
                     placeholder="username"
@@ -50,7 +89,8 @@ const Register = (props) => {
                 />
                 </div>
                 <button className="log-reg-button">Register</button>
-            </form>
+            </form> */}
+
             </Formik>    
             <div className="info-text">Already registered? Go <Link to="/">here</Link></div>
         </div>
